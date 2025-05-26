c7ef458d2c4eb202df043a857ec0464ae9deec7e
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-05-26 15:54:47 +0500

    feat: add handler and queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, gqlRootSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -15,7 +15,16 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      const { query, variables } = req.body;
+
+      const result = await graphql({
+        source: query,
+        variableValues: variables,
+        schema: gqlRootSchema,
+        contextValue: { prisma },
+      });
+
+      return result;
     },
   });
 };
---
+++ src/routes/graphql/queries.ts
@@ -0,0 +1,116 @@
+import { GraphQLNonNull, GraphQLList, GraphQLObjectType } from 'graphql';
+import {
+  PostType,
+  UserType,
+  ProfileType,
+  GqlContext,
+  MemberType,
+  MemberTypeId,
+} from './types/types.js';
+import { UUIDType } from './types/uuid.js';
+import {
+  parseResolveInfo,
+  ResolveTree,
+  simplifyParsedResolveInfoFragmentWithType,
+} from 'graphql-parse-resolve-info';
+import { MemberTypeIdType } from './types/enums.js';
+
+export const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: async (_, __, context: GqlContext, info) => {
+        const resolveTree = parseResolveInfo(info) as ResolveTree;
+        const { fields } = simplifyParsedResolveInfoFragmentWithType(
+          resolveTree,
+          info.returnType,
+        );
+
+        const include: {
+          subscribedToUser?: boolean;
+          userSubscribedTo?: boolean;
+        } = {};
+
+        if (Object.prototype.hasOwnProperty.call(fields, 'subscribedToUser')) {
+          include.subscribedToUser = true;
+        }
+
+        if (Object.prototype.hasOwnProperty.call(fields, 'userSubscribedTo')) {
+          include.userSubscribedTo = true;
+        }
+
+        return context.prisma.user.findMany({ include });
+      },
+    },
+
+    user: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_, { id }: { id: string }, context: GqlContext) => {
+        return context.prisma.user.findUnique({
+          where: { id },
+        });
+      },
+    },
+
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      resolve: (_, __, context: GqlContext) => {
+        return context.prisma.profile.findMany();
+      },
+    },
+
+    profile: {
+      type: ProfileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: (_, { id }: { id: string }, context: GqlContext) => {
+        return context.prisma.profile.findUnique({
+          where: { id },
+        });
+      },
+    },
+
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: (_, __, context: GqlContext) => {
+        return context.prisma.post.findMany();
+      },
+    },
+
+    post: {
+      type: PostType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: (_, { id }: { id: string }, context: GqlContext) => {
+        return context.prisma.post.findUnique({
+          where: { id },
+        });
+      },
+    },
+
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      resolve: (_, __, context: GqlContext) => {
+        return context.prisma.memberType.findMany();
+      },
+    },
+
+    memberType: {
+      type: MemberType,
+      args: {
+        id: { type: new GraphQLNonNull(MemberTypeId) },
+      },
+      resolve: (_, { id }: { id: MemberTypeIdType }, context: GqlContext) => {
+        return context.prisma.memberType.findUnique({
+          where: { id },
+        });
+      },
+    },
+  },
+});
