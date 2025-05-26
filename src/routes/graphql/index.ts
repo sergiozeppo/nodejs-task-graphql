6214507b19755c1cf955f71ee65614754e4a31d1
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-05-26 20:08:19 +0500

    feat: limit complexity of the graphql queries by their depth

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, gqlRootSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -14,8 +15,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
+    async handler(req, res) {
       const { query, variables } = req.body;
+
+      const errorsValidate = validate(gqlRootSchema, parse(query), [depthLimit(5)]);
+      if (errorsValidate.length > 0) {
+        await res.send({ errors: errorsValidate });
+      }
 
       const result = await graphql({
         source: query,
